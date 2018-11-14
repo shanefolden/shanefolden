e4e0d9a131d264f44ef2d4c80df51485da94fc2d
supercarlos297 <footballshane@gmail.com>
2018-11-14 03:05:13 -0800

    contact page construction

---
+++ sfolden.js
@@ -0,0 +1,19 @@
+setLogoWidth();
+function setLogoWidth()
+{
+
+  var screenWidth = window.innerWidth;
+  var logoWidth = document.getElementById("logo").width;
+  var innerMar = parseInt( $("#logo").css("marginLeft") );
+  var width = 5*logoWidth + 8*innerMar;
+  var space = (screenWidth-width)/2;
+  document.getElementById("leftLogo").style.marginLeft = space+"px";
+  document.getElementById("rightLogo").style.marginRight = space+"px";
+
+
+  console.log(logoWidth);
+  console.log(innerMar);
+  console.log(space);
+
+
+}
