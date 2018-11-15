6c539cbc2b432c4d69fb621d58be6eb80861973d
supercarlos297 <footballshane@gmail.com>
2018-11-15 01:03:40 -0800

    SCROLL CIRCLE LOOKS REALLY COOL

--- index.js
+++ index.js
@@ -29,11 +29,17 @@ var topmar = screenHeight-clientHeight;
 
 }
 window.addEventListener("scroll", function (event) {
+
     var scroll = this.scrollY;
-    document.getElementById("circle").style.width = 2500-4*scroll+"px";
-    document.getElementById("circle").style.height = 2500-4*scroll+"px";
-    document.getElementById("circle").style.top = -200+1.2*scroll+"px";
-    document.getElementById("circle").style.left = -600+1.92*scroll+"px";
+    if(scroll<520)
+    {
+      document.getElementById("circle").style.width = 2500-4*scroll+"px";
+      document.getElementById("circle").style.height = 2500-4*scroll+"px";
+      document.getElementById("circle").style.top = -200+1.45*scroll+"px";
+      document.getElementById("circle").style.left = -600+1.92*scroll+"px";
+    }
+
+
 
 
 
