4b8d7fadb50aeaddd2263895e28e7c7a0b37d974
supercarlos297 <footballshane@gmail.com>
2018-11-15 12:08:03 -0800

    good color

--- index.js
+++ index.js
@@ -28,22 +28,44 @@ var topmar = screenHeight-clientHeight;
   //console.log(document.getElementById("jsgod").style.marginTop)
 
 }
+
+window.addEventListener("resize", function (event) {
+  var screenWidth = window.innerWidth;
+  var widthCheck = screenWidth/2
+  var cirWidth = parseInt( $("#circle").css("width"));
+  document.getElementById("circle").style.left = widthCheck-cirWidth/2+"px";
+
+});
+
 window.addEventListener("scroll", function (event) {
 
+
     var scroll = this.scrollY;
+    var screenWidth = window.innerWidth;
+    var widthCheck = screenWidth/2
+    var cirWidth = parseInt( $("#circle").css("width"));
     if(scroll<520)
     {
       document.getElementById("circle").style.width = 2500-4*scroll+"px";
       document.getElementById("circle").style.height = 2500-4*scroll+"px";
       document.getElementById("circle").style.top = -200+1.45*scroll+"px";
-      document.getElementById("circle").style.left = -600+1.92*scroll+"px";
+      
+      document.getElementById("circle").style.left = widthCheck-cirWidth/2+"px";
     }
 
     if(scroll>520)
     {
+      var newScroll = (scroll-521);
       var element = document.getElementById("plx");
-      element.setAttribute("data-speed", "2.0");
+
+      element.classList.remove("parallax-window");
+      document.getElementById("circle").style.top = -200+1.45*521+newScroll/2+"px";
+      document.getElementById("circle").style.width = 420+"px";
+      document.getElementById("circle").style.height = 420+"px";
+      document.getElementById("circle").style.left = widthCheck-cirWidth/2+"px";
+
     }
+
 
 
 
