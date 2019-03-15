8036d605d5645560ad7882b57bfbee14ae9a5755
supercarlos297 <footballshane@gmail.com>
2019-03-15 00:35:02 -0700

    close

--- lights.js
+++ lights.js
@@ -1,3 +1,5 @@
 if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Hey just so you know any messages you send wont go through when you use safari, and I havent figured out why yet. Chrome or firefox will work though');
 
 };
+
+alert('ok so its kinda broken temporarily it should be fixed in like a day or so ')
