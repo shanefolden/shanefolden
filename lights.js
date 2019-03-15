a3fac3bd77621d315ea2cf19bf661fffc8a95431
supercarlos297 <footballshane@gmail.com>
2019-03-15 02:17:34 -0700

    deleted site down alert

--- lights.js
+++ lights.js
@@ -1,5 +1,3 @@
 if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Hey just so you know any messages you send wont go through when you use safari, and I havent figured out why yet. Chrome or firefox will work though');
 
 };
-
-alert('ok so its kinda broken temporarily it should be fixed in like an hour or so ');
