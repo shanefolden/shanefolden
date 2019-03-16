52a6a92e55f4d9d11ec7eb2eeabc673001d75bf1
supercarlos297 <footballshane@gmail.com>
2019-03-15 21:46:24 -0700

    removed safari alert

--- lights.js
+++
@@ -1,3 +0,0 @@
-if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Hey just so you know any messages you send wont go through when you use safari, and I havent figured out why yet. Chrome or firefox will work though');
-
-};
