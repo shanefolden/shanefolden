e7f7a7e5bb9baa91d2b9e973064829e5cf4c8764
supercarlos297 <footballshane@gmail.com>
2019-10-17 23:15:13 -0700

    it works a bit better now

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 /*jslint browser:true */
 
-if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {//alert('Hey just so you know my site is kinda broken on safari, chrome is a way better option');
+//if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {//alert('Hey just so you know my site is kinda broken on safari, chrome is a way better option');
 
-}
+//}
 
 
 
@@ -17,18 +17,32 @@ function fixPad() {
 // var screenWidth = window.innerWidth, widthCheck = screenWidth / 2, cirWidth = parseInt($("#circle").css("width"));
 // document.getElementById("circle").style.left = widthCheck - cirWidth / 2 + "px";
 
+var other = anime({
+
+
+})
 
 anime({
-  targets: 'div.hole',
+  targets: 'div.cropimage',
+  borderRadius: ['0%', '50%'],
+  width: ['100%', '30%'],
+  height: ['100%', '50%'],
+  marginTop: ['0%', '50%'],
+  marginBottom: ['0%', '20%'],
 
-  scale: .2,
+  marginRight:  ['0%', '35%'],
+  marginLeft: ['0%', '35%'],
 
+
+  delay: 1000,
+
+  //psuedocode
   duration: 3000
 
   //console.log(width);
 })
 
-anime();
+//anime();
 
 
 
---
+++ index2.js
@@ -0,0 +1,13 @@
+anime({
+  targets: 'div.indexBackground',
+
+  marginTop: ['0%', '-40%'],
+
+
+  delay: 1500,
+
+  //psuedocode
+  duration: 3000
+
+
+})
