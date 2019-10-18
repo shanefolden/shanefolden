e2aba9b2054df8bee30c75721e594e17875ae70e
supercarlos297 <footballshane@gmail.com>
2019-10-17 18:46:43 -0700

    if everything breaks go back to this version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 /*jslint browser:true */
 
-if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Hey just so you know my site is kinda broken on safari, chrome is a way better option');
+if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {//alert('Hey just so you know my site is kinda broken on safari, chrome is a way better option');
 
 }
 
@@ -13,16 +13,33 @@ function fixPad() {
   //console.log(document.getElementById("jsgod").style.marginTop)
 
 }
-fixPad();
-var screenWidth = window.innerWidth, widthCheck = screenWidth / 2, cirWidth = parseInt($("#circle").css("width"));
-document.getElementById("circle").style.left = widthCheck - cirWidth / 2 + "px";
-
-window.addEventListener("resize", function (event) {
-
-  screenWidth = window.innerWidth;
-  widthCheck = screenWidth / 2;
-  cirWidth = parseInt($("#circle").css("width"));
-  document.getElementById("circle").style.left = widthCheck - cirWidth / 2 + "px";
+// fixPad();
+// var screenWidth = window.innerWidth, widthCheck = screenWidth / 2, cirWidth = parseInt($("#circle").css("width"));
+// document.getElementById("circle").style.left = widthCheck - cirWidth / 2 + "px";
 
 
-});
+anime({
+  targets: 'div.hole',
+
+  scale: .2,
+
+  duration: 3000
+
+  //console.log(width);
+})
+
+anime();
+
+
+
+
+// // resizes circle if page is resized
+// window.addEventListener("resize", function (event) {
+//
+//   screenWidth = window.innerWidth;
+//   widthCheck = screenWidth / 2;
+//   cirWidth = parseInt($("#circle").css("width"));
+//   document.getElementById("circle").style.left = widthCheck - cirWidth / 2 + "px";
+//
+//
+// });
